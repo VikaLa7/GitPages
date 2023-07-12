8786ca1eae8a8a60c7fb524a233622e743baa18b
VikaLa7 <118175728+VikaLa7@users.noreply.github.com>
2023-07-12 13:31:26 +0000

    Deploying to gh-pages from @ VikaLa7/GitPages@ffbe84413863b095737673f268a09e8c8f38e558 🚀

--- _app/immutable/chunks/paths.4da7b827.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1sk64ij)==null?void 0:s.base)??"/https://github.com/VikaLa7/GitPages";var t;const e=((t=globalThis.__sveltekit_1sk64ij)==null?void 0:t.assets)??a;export{e as a,a as b};
